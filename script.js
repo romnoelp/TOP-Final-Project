53e9af7ff0ba0564b0e8446bf0684bfc146cdfa8
Romnoel Petracorta <petracorta.rmnl@gmail.com>
2024-09-05 14:55:12 +0800

    Numerical buttons
    Selected the buttons that have the span tag inside of them i.e. numerical buttons.

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-// Button declations
-const buttons = document.querySelectorAll("button")
-const numericalButtons = Array.from(buttons).filter(button => !button.querySelector('span'));
-let value = "";
-numericalButtons.forEach(element => {
-    element.addEventListener("click", () => {
-        value = element.innerHTML;
-    })
+// Button declations.
+const buttons = document.querySelectorAll("button");
+const calculatorScreen = document.querySelector(".screen");
+
+// Selects all the buttons that doesn't have the span tag inside.
+const numericalButtons = Array.from(buttons).filter(
+   (button) => !button.querySelector("span")
+);
+let firstAddend = 0;
+let secondAdded = 0;
+
+// On click event for the numerical buttons.
+numericalButtons.forEach((element) => {
+   element.addEventListener("click", () => {
+      let firstValue = document.createElement("p");
+      firstValue.style.color = "#edf1f4";
+      firstValue.textContent = element.innerText;
+      console.log(firstValue);
+
+      calculatorScreen.appendChild(firstValue);
+   });
 });
-
-
