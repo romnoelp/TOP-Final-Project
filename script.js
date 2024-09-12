5f408d3c25f3d57dbb85e9291f3618eccf7164fa
romnoelp <petracorta.rmnl@gmail.com>
2024-09-12 18:47:46 +0800

    Fixed the bug where it won't delete the contents of the calculator screen.

--- script.js
+++ script.js
@@ -1,88 +1,50 @@
-const buttons = document.querySelectorAll("button");
+// Buttons
+const numericalButtons = document.querySelectorAll(
+   "button:not(.equal):not(:has(span))"
+);
 const calculatorScreen = document.querySelector(".screen");
-
-let firstAddend = 0;
-let newValue = "";
-let newOperationalValue = "";
-let newLastValue = "";
-let existingPElement = null;
-let existingOPElement = null;
-let existingLastPElement = null;
-
-let operationalButtonClicked = false; // Flag to track if an operational button has been clicked.
-
-// Selects all the buttons that don't have the span tag inside.
-const firstNumericalButton = Array.from(buttons).filter(
-   (button) => !button.querySelector("span")
+// Operators
+const operationalButtons = document.querySelectorAll(
+   "button:not(.equal):has(span)"
 );
 
-const operationalButtons = Array.from(buttons).filter((button) =>
-   button.querySelector("span")
-);
+const equalButton = document.querySelector(".equal");
 
-const secondNumericalButton = Array.from(buttons).filter(
-   (button) => !button.querySelector("span")
-);
+let firstNumber = 0;
+let operator = "";
+let secondNumber = 0;
+let result = 0;
+let expression = "";
 
-function handleFirstNumericalButtonClick(event) {
-   if (!existingPElement) {
-      // If there's no existing <p> element, create one and add it to the screen.
-      existingPElement = document.createElement("p");
-      existingPElement.style.color = "#edf1f4";
-      calculatorScreen.appendChild(existingPElement);
-   }
-
-   newValue += event.target.innerText;
-   existingPElement.textContent = newValue;
-}
-
-// Attach the function as the event listener to the numerical buttons.
-firstNumericalButton.forEach((element) => {
-   element.addEventListener("click", handleFirstNumericalButtonClick);
+numericalButtons.forEach((button) => {
+   button.addEventListener("click", () => {
+      const element = document.createElement("p");
+      element.textContent = button.textContent;
+      expression += button.textContent;
+      console.log(expression);
+      calculatorScreen.appendChild(element);
+   });
 });
 
-function handleOperationalButtonClick(event) {
-   if (!existingOPElement) {
-      existingOPElement = document.createElement("p");
-      existingOPElement.style.color = "#f5a814";
-      calculatorScreen.appendChild(existingOPElement);
-   }
-
-   newOperationalValue += event.target.innerText;
-   existingOPElement.textContent = newOperationalValue;
-
-   // Disable numerical buttons after an operational button is pressed.
-   firstNumericalButton.forEach((element) => {
-      element.removeEventListener("click", handleFirstNumericalButtonClick);
+operationalButtons.forEach((button) => {
+   button.addEventListener("click", () => {
+      const element = document.createElement("p");
+      element.textContent = button.textContent;
+      element.style.color = "#f5a814";
+      expression += button.textContent;
+      calculatorScreen.appendChild(element);
    });
-
-   // Activate second numerical buttons.
-   operationalButtonClicked = true;
-   secondNumericalButton.forEach((element) => {
-      if (!element.hasAttribute("data-clicked")) {
-         element.addEventListener("click", handleSecondNumericalButtonClick);
-         element.setAttribute("data-clicked", "true");
-      }
-   });
-}
-
-// Attach the function as the event listener to the operational buttons.
-operationalButtons.forEach((element) => {
-   element.addEventListener("click", handleOperationalButtonClick);
 });
 
-function handleSecondNumericalButtonClick(event) {
-   if (!existingLastPElement) {
-      existingLastPElement = document.createElement("p");
-      existingLastPElement.style.color = "#edf1f4";
-      calculatorScreen.appendChild(existingLastPElement);
+equalButton.addEventListener("click", () => {
+   result = eval(expression);
+   
+   while (calculatorScreen.firstChild) {
+      calculatorScreen.removeChild(calculatorScreen.firstChild);
    }
 
-   newLastValue += event.target.innerText;
-   existingLastPElement.textContent = newLastValue;
-
-   // Disable second numerical buttons after they are clicked.
-   secondNumericalButton.forEach((element) => {
-      element.removeEventListener("click", handleSecondNumericalButtonClick);
-   });
-}
+   const resultElement = document.createElement("p");
+   resultElement.textContent = result;
+   calculatorScreen.appendChild(resultElement);
+   expression = "";   
+});
